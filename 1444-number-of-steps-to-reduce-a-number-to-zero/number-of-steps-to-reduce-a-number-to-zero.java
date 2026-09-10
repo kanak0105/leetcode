9cd1a58ecf94426989e440467bd610a6acf451e9
Kanak Rajput <kanakr0105@gmail.com>
2026-09-10 11:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (18.22%) - LeetSync

--- 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,14 +1,14 @@
 class Solution {
     public int numberOfSteps(int num) {
-        return helper(num, 0 );
+        return helper(num,0);
     }
-    private int helper(int num , int count){
-        if(num==0){
-            return count;
+    static int helper(int num , int steps){
+        if (num == 0 ){
+            return steps ;
         }
-        if (num%2 == 0){
-            return helper(num/2,count+1);
+        if(num%2==0){
+            return helper(num/2,steps+1);
         }
-        return helper(num-1,count+1);
+        return helper(num-1,steps+1);
     }
 }
