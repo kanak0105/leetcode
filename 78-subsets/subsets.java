a350585960572be02577fa78619941828d3462ac
Kanak Rajput <kanakr0105@gmail.com>
2026-09-14 12:12:01 +0530

    Time: 1 ms (87.18%) | Memory: 44.2 MB (56.84%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        int n = nums.length;
+       /* int n = nums.length;
         List<List<Integer>> finalans = new ArrayList<>(); 
         for(int i = 0; i <(1<< n) ; i++){
             ArrayList<Integer> temp = new ArrayList<>();
@@ -11,6 +11,21 @@ class Solution {
             }
             finalans.add(temp);
         }
-        return finalans;  
-    }
-}
+        return finalans;  */
+      
+      List<List<Integer>> outer = new ArrayList<>();
+
+      outer.add(new ArrayList());
+
+      for (int num : nums) {
+          int n = outer.size();
+          for (int i = 0; i < n; i++) {
+              List<Integer> internal = new ArrayList<>(outer.get(i));
+              internal.add(num);
+              outer.add(internal);
+          }
+      }
+          return outer;
+      }
+  }
+  
